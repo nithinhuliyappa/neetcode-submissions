8e0d67cc644c05440b84a26c967be70cc58dc35c
Nithin Huliyappa <46097994+nithinhuliyappa@users.noreply.github.com>
2026-06-23 10:39:08 -0600

    Add: merge-intervals - submission-0

---
+++ Data Structures & Algorithms/merge-intervals/submission-0.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a,b)->Integer.compare(a[0], b[0]));
+        List<int[]> res = new ArrayList<>();
+        res.add(intervals[0]);
+
+        for(int i = 1; i < intervals.length; i++){
+            int[] prev = res.get(res.size()-1);
+            int[] cur = intervals[i];
+
+            if(prev[1] >= cur[0]){
+                prev[1] = Math.max(prev[1], cur[1]);
+            } else {
+                res.add(cur);
+            }
+        }
+
+        return res.toArray(new int[res.size()][]);
+    }
+}
